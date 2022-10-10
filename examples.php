91a3ef42febc9dcbc6fb34c1cb4c2bcb48ca0254
marciv <barlot.marc@gmail.com>
2022-10-10 23:00:51 +0200

    implement simple middleware
    implement simple middleware

--- examples.php
+++ examples.php
@@ -16,9 +16,25 @@ require __DIR__ . '/vendor/autoload.php';
 $httpRequest = new HttpRequest();
 $router = new Router($httpRequest);
 use Georges\Controllers\Home;
-$router::get('/','Home','home');
-// echo '<pre>';
-// $listRoute = $router->getListRoute();
-// print_r($listRoute);
-// echo '</pre>';
+
+
+// Declare a custom middleware function
+// if empty return middleware chaining is stopped
+function Home1 ($httpRequest){    
+    
+    return 'middleware1';
+}
+
+// implement middleware and add closure function as a middleware
+$router->use('/','Home1',function(){
+    
+    return 'middleware2';
+});
+// middleware return are store in HttpRequest object, I.E for example bellow in controller $HttpRequest->middleware->home1 = true
+print_r($httpRequest->middleware['Home1']);
+
+// define a new route as static var in router object
+$router::get('/','Home','home',['prenom'=>"marc"]);
+
+// exectue route by router found
 $router->run();
